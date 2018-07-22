0e46c70604e639217cd46fb68a86700e15e7081c
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2018-07-22 17:23:25 +0500

    Top button was added

--- js/main.js
+++ js/main.js
@@ -1,42 +1,57 @@
 //Галерея 
 
 function openModal() {
-  document.getElementById('myModal').style.display = "block";
+    document.getElementById('myModal').style.display = "block";
 }
 
 function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+    document.getElementById('myModal').style.display = "none";
 }
 
 var modalIndex = 1;
 showModals(modalIndex);
 
 function plusSlides(n) {
-  showModals(modalIndex += n);
+    showModals(modalIndex += n);
 }
 
 function currentSlide(n) {
-  showModals(modalIndex = n);
+    showModals(modalIndex = n);
 }
 
 function showModals(n) {
-  var i;
-  var slides = document.getElementsByClassName("myModals");
-  if (n > slides.length) {modalIndex = 1}
-  if (n < 1) {modalIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
+    var i;
+    var slides = document.getElementsByClassName("myModals");
+    if (n > slides.length) {
+        modalIndex = 1
+    }
+    if (n < 1) {
+        modalIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-  slides[modalIndex-1].style.display = "block";
+    slides[modalIndex - 1].style.display = "block";
 }
 
 
-$(document).ready(function() {
- $(".fancybox").fancybox({
-    openEffect	: 'none',
-    closeEffect	: 'none'
-  });
+$(document).ready(function () {
+    $(".fancybox").fancybox({
+        openEffect: 'none',
+        closeEffect: 'none'
+    });
 
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > $(this).height()) {
+            $(".top").addClass("active");
+        } else {
+            $(".top").removeClass("active");
+        }
+    });
+
+    $("body").on("click", ".top", function () {
+        $("html, body").animate({scrollTop: 0}, "slow");
+    });
 });
